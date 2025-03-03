e8dff35437336bddd0b5a6f897610d8af9a8db6d
Kenzie-Nice <157401036+Kenzie-Nice@users.noreply.github.com>
2025-03-03 15:59:35 -0700

    Update viewmaster.js

--- viewmaster.js
+++ viewmaster.js
@@ -12,8 +12,8 @@ const slides = [
     new PhotoSlide("Freedom", "141104-berlin-wall-neely-cover.jpg", "Piece of berlin wall getting torn down", "Dorothea Lange", "1989"),
     new PhotoSlide("Broken", "The-first-section-of-the-Berlin-Wall-was-torn-down-by-crowds-on-the-morning-of-November-10th-1989-530033.jpg", "Berlin Wall", "Unknown", "1989"),
     new PhotoSlide("Spray", "xxberlin-wall-archive-videoSixteenByNineJumbo1600.jpg", "Unknown", "1989"),
-    new PhotoSlide("Stand", "030-56a040383df78cafdaa0adf6.jpg", "The famous Tank-man photo, taken during the Tiananmen protests ""John Doe", "1989"),
-    new PhotoSlide("Flag", "01_VIME4375-1989CL12.jpg" " Someone holds a red and gold flag as the winds rip by at the Tiananmen protests""Jhon doe","" "1989")
+    new PhotoSlide("Stand", "030-56a040383df78cafdaa0adf6.jpg", "The famous Tank-man photo, taken during the Tiananmen protests", "John Doe", "1989"),
+    new PhotoSlide("Flag", "01_VIME4375-1989CL12.jpg", " Someone holds a red and gold flag as the winds rip by at the Tiananmen protests", "Jhon doe", "1989")
 ];
 
 function preloadImages() {
